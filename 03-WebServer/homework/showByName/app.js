efc4879d8eebca2e277534697c31cd7800c4a3ea
Family - NC <hendri.naas@gmail.com>
2022-09-18 08:55:37 -0300

    Homework Complete

--- 03-WebServer/homework/showByName/app.js
+++ 03-WebServer/homework/showByName/app.js
@@ -3,14 +3,17 @@ var http  = require("http")
 
 // Escribí acá tu servidor
 http.createServer((req, res) => {
+  //Leemos el archivo para encontrar la imagen indicada
   fs.readFile(`${__dirname}/images/${req.url}.jpg`, (err, data) => {
     if(err) {
+      //Respondemos con un error si no existe 
       res.writeHead(404, {"Content-type": "text/plain"});
       res.end("Hubo un error, el pero no existe");
     }else {
-      res.writeHead(200, {"Content-type": "image/jpg"})
+      //Respondemos enviado la imagen si se encuentra dentro del archivo
+      res.writeHead(200, {"Content-type": "image/jpg"});  
       res.end(data);
     }
   });
 
-}).listen(3000, "127.0.0.1");
+}).listen(3000, "127.0.0.1"); //Estabeciendo el puerto y la IP por defecto de localhost.
--- 04-AdvancePromises/homework/src/pledge.js
+++ 04-AdvancePromises/homework/src/pledge.js
@@ -2,9 +2,96 @@
 /*----------------------------------------------------------------
 Promises Workshop: construye la libreria de ES6 promises, pledge.js
 ----------------------------------------------------------------*/
-// // TU CÓDIGO AQUÍ:
+// TU CÓDIGO AQUÍ:
+function $Promise(executor){
+  if(typeof executor !== 'function') throw new TypeError('the executor must be a function');
+  this._state = 'pending';
+  this._handlerGroups = [];
+  executor(this._internalResolve.bind(this), this._internalReject.bind(this));
+}
 
+$Promise.prototype._internalResolve = function(value){
+  if(this._state === 'pending'){
+    this._state = 'fulfilled';
+    this._value = value;
+    this._callHandlers();
+  }
+}
+$Promise.prototype._internalReject = function(reason){
+  if(this._state === 'pending'){
+    this._state = 'rejected';
+    this._value = reason;
+    this._callHandlers();
+  }
+}
+$Promise.prototype._callHandlers = function(){
+  while(this._handlerGroups.length){
+    const hd = this._handlerGroups.shift();
+    if(this._state === 'fulfilled') {
+      if(hd.successCb) {
+        try {
+          const result = hd.successCb(this._value);
+          if(result instanceof $Promise){
+            // Handler devuelve una promesa
+            result.then(
+              value => hd.downstreamPromise._internalResolve(value),
+              error => hd.downstreamPromise._internalReject(error)
+            )
+          }else {
+            //Handler devolvió un valor
+            hd.downstreamPromise._internalResolve(result);
+          }
+        } catch (error) { 
+          //Handler arrojó un error/excepción
+          hd.downstreamPromise._internalReject(error);
+        }
+      }else {
+        hd.downstreamPromise._internalResolve(this._value);
+      }
+    }
+    else if(this._state === 'rejected'){
+      if(hd.errorCb){
+        try {
+          const result = hd.errorCb(this._value);
+          if(result instanceof $Promise){
+            // Handler devuelve una promesa
+            result.then(
+              value => hd.downstreamPromise._internalResolve(value),
+              error => hd.downstreamPromise._internalReject(error)
+            )
+          }else {
+            //Handler devolvió un valor
+            hd.downstreamPromise._internalResolve(result);
+          }
+        } catch (error) {
+          //Handler arrojó un error/excepción
+          hd.downstreamPromise._internalReject(error);
+        }
+      }else {
+        hd.downstreamPromise._internalReject(this._value);
+      }
+    }
+  }
+}
 
+$Promise.prototype.then = function(successCb, errorCb){
+  if(typeof successCb !== 'function') successCb = false;
+  if(typeof errorCb !== 'function') errorCb = false;
+
+  const downstreamPromise = new $Promise(()=>{});
+  this._handlerGroups.push({
+    successCb,
+    errorCb,
+    downstreamPromise
+  });
+
+  if(this._state !== 'pending') this._callHandlers();
+  return downstreamPromise;
+}
+
+$Promise.prototype.catch = function(errorCb){
+  return this.then(null, errorCb);
+}
 
 module.exports = $Promise;
 /*-------------------------------------------------------
